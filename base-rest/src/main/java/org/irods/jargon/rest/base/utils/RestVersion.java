a772cdd915a4d5283b048a4712ba20715d35fec8
michael-conway <michael_conway@unc.edu>
2017-02-14 08:14:04 -0500

    #2 initial tests of spring wiring

---
+++ base-rest/src/main/java/org/irods/jargon/rest/base/utils/RestVersion.java
@@ -0,0 +1,5 @@
+package org.irods.jargon.rest.base.utils;
+public final class RestVersion {
+ public static String VERSION="4.2.1.0-SNAPSHOT";
+ public static String BUILD_TIME="2017-02-14T12:59:28Z";
+}
