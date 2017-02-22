a3b726b10c41bf861d8b7be07084c34c586b8b38
michael-conway <michael_conway@unc.edu>
2017-02-22 13:24:22 -0500

    #1 allow to find pom at renci

--- base-rest/src/main/java/org/irods/jargon/rest/base/utils/RestVersion.java
+++ base-rest/src/main/java/org/irods/jargon/rest/base/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.rest.base.utils;
 public final class RestVersion {
  public static String VERSION="4.2.1.0-SNAPSHOT";
- public static String BUILD_TIME="2017-02-15T14:10:07Z";
+ public static String BUILD_TIME="2017-02-22T18:23:47Z";
 }
