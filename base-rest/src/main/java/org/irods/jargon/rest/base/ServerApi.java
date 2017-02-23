e8632b98f032b14e5696f29667a27aa515beb117
michael-conway <michael_conway@unc.edu>
2017-02-23 15:54:48 -0500

    #4 works with ping

--- base-rest/src/main/java/org/irods/jargon/rest/base/ServerApi.java
+++ base-rest/src/main/java/org/irods/jargon/rest/base/ServerApi.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
+import org.irods.jargon.rest.exception.IrodsRestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,7 +23,7 @@ public class ServerApi {
 	@Path("/ping")
 	@Produces({ "application/json", "application/xml" })
 	public Response server(@QueryParam("midTierOnly") Boolean midTierOnly,
-			@Context SecurityContext securityContext) throws NotFoundException {
+			@Context SecurityContext securityContext) throws IrodsRestException {
 		return serverApiService.server(midTierOnly, securityContext);
 	}
 
--- base-service-impl/src/main/java/org/irods/jargon/rest/base/ServerApiService.java
+++ base-service-impl/src/main/java/org/irods/jargon/rest/base/ServerApiService.java
@@ -3,11 +3,16 @@ package org.irods.jargon.rest.base;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
+import org.irods.jargon.core.exception.JargonException;
+import org.irods.jargon.core.pub.EnvironmentalInfoAO;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
 import org.irods.jargon.rest.base.model.Ping;
+import org.irods.jargon.rest.exception.IrodsRestException;
+import org.irods.jargon.rest.security.IrodsAuthentication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -30,7 +35,7 @@ public class ServerApiService {
 	 * @throws NotFoundException
 	 */
 	public Response server(Boolean midTierOnly, SecurityContext securityContext)
-			throws NotFoundException {
+			throws IrodsRestException {
 
 		log.info("server()");
 		if (midTierOnly == null) {
@@ -41,10 +46,31 @@ public class ServerApiService {
 			throw new IllegalArgumentException("null securityContext");
 		}
 
-		log.info("securityContext:{}", securityContext);
+		log.info("authentication:{}", SecurityContextHolder.getContext()
+				.getAuthentication());
+		IrodsAuthentication irodsAuthentication = (IrodsAuthentication) SecurityContextHolder
+				.getContext().getAuthentication();
 
 		float millis = 0.0f;
 		if (!midTierOnly) {
+			log.info("doing a ping!");
+			log.info("authentication:{}", securityContext.getUserPrincipal());
+			long startTime = System.currentTimeMillis();
+			log.info("startTime:{}", startTime);
+			try {
+				log.info("accessing irods");
+				EnvironmentalInfoAO environmentalInfoAO = irodsAccessObjectFactory
+						.getEnvironmentalInfoAO(irodsAuthentication
+								.getIrodsAccount());
+				environmentalInfoAO.getIRODSServerCurrentTime();
+				log.info("got properties");
+			} catch (JargonException e) {
+				log.error("Error pinging back end irods server", e);
+				throw new IrodsRestException(e);
+			}
+			long endTime = System.currentTimeMillis();
+			millis = endTime - startTime;
+			log.info("endTime:{}", endTime);
 
 		}
 
