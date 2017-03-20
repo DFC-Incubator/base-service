6594b0542f429395ff8faf885fa734706edc675d
michael-conway <michael_conway@unc.edu>
2017-03-20 09:35:25 -0400

    #5 spring enable

--- base-rest/src/main/java/org/irods/jargon/rest/base/utils/RestVersion.java
+++ base-rest/src/main/java/org/irods/jargon/rest/base/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.rest.base.utils;
 public final class RestVersion {
  public static String VERSION="4.2.1.0-SNAPSHOT";
- public static String BUILD_TIME="2017-03-20T11:47:21Z";
+ public static String BUILD_TIME="2017-03-20T13:15:43Z";
 }
